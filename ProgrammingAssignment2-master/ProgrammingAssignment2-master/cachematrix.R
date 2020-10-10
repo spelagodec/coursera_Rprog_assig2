9fd0631980491e87be086319680b8371b6db6714
Spela Godec <ucrhgod@ucl.ac.uk>
2020-10-10 21:35:38 +0100

    committing inverse functions

---
+++ ProgrammingAssignment2-master/ProgrammingAssignment2-master/cachematrix.R
@@ -0,0 +1,34 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## function creates a makeCacheMatrix matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set,
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+}
+
+
+## cacheSolve inverses a matrix, excluding null values
+
+cacheSolve <- function(x, ...) {
+        i <- x$getinverse()
+    if (!is.null(i)) {
+      message("getting cached data")
+      return(i)
+    }
+    data <- x$get()
+    i <- solve(data, ...)
+    x$setinverse(i)
+    i
+}
